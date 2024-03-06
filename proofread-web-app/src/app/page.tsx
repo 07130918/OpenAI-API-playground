9efb64e4b6ffa7882946e637bff529dcc9ae9468
こた <tokyosato1@gmail.com>
2024-03-06 17:21:17 +0900

    feat: 高さ修正

--- proofread-web-app/src/app/page.tsx
+++ proofread-web-app/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Button, Flex, Heading, Input, Textarea, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Input, Textarea } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 import { journal } from '@/lib/templates';
@@ -12,12 +12,10 @@ const Home: NextPage = () => {
     const { journal, setJournal, secret, setSecret, proofread, handleSubmit, isLoading } =
         useProofreading(formattedJournal);
 
-    const boxHeight = useBreakpointValue({ base: 'auto', md: '100vh' });
-
     return (
         <Flex
             bgGradient='linear(to-r, black, blue.900)'
-            height={boxHeight}
+            minH='100vh'
             justify='center'
             align='center'
         >
@@ -27,42 +25,32 @@ const Home: NextPage = () => {
                 borderRadius='lg'
                 shadow='lg'
                 p={8}
-                overflowY={boxHeight === 'auto' ? 'scroll' : 'visible'}
+                overflowY='scroll'
             >
                 <Box
-                    as='h1'
+                    as='h2'
                     fontSize='3xl'
                     fontWeight='bold'
                     background='linear-gradient(to right, #ff00cc, #3333cc)'
                     bgClip='text'
-                    mb={6}
+                    pb={2}
                 >
                     Journal Proofreading
                 </Box>
                 <Flex direction={['column', 'column', 'row']} gap={6}>
                     <Box flex={1}>
                         <Heading size='md' mb={2}>
-                            Your OpenAI API key or Password:
-                        </Heading>
-                        <Input
-                            value={secret}
-                            onChange={(e) => setSecret(e.target.value)}
-                            placeholder='Enter your OpenAI API key or password here'
-                            size='lg'
-                            mb={4}
-                            type='password'
-                        />
-                        <Heading size='md' mb={2}>
                             Journal Entry:
                         </Heading>
-                        <Textarea
-                            value={journal}
-                            onChange={(e) => setJournal(e.target.value)}
-                            placeholder='Enter your journal here'
-                            size='lg'
-                            mb={4}
-                            minHeight='40vh'
-                        />
+                        <Box pb={4}>
+                            <Textarea
+                                value={journal}
+                                onChange={(e) => setJournal(e.target.value)}
+                                placeholder='Enter your journal here'
+                                size='lg'
+                                rows={18}
+                            />
+                        </Box>
                         <Button
                             width='full'
                             colorScheme='teal'
@@ -76,7 +64,12 @@ const Home: NextPage = () => {
                         <Heading size='md' mb={2}>
                             Proofread Text:
                         </Heading>
-                        <Textarea size='lg' minHeight='60vh' isReadOnly value={proofread} />
+                        <Textarea
+                            size='lg'
+                            // minHeight='60vh'
+                            isReadOnly
+                            value={proofread}
+                        />
                     </Box>
                 </Flex>
             </Box>
